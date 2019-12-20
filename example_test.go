b81512c60874015e211452c6507e6a26fdb806f7
scizorman <tueda1207@gmail.com>
2019-12-21 03:08:40 +0900

    [add] Example with special characters

--- example_test.go
+++ example_test.go
@@ -24,3 +24,13 @@ func Example_number() {
 	fmt.Println(s)
 	// Output: 97301642757693321863
 }
+
+func Example_with_special_characters() {
+	// For testing, set the seed to 1.
+	// So please use it without `randstr.WithRand()` if you do not set a specific seed.
+	chars := `abcdefgihijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@%+\/'!#$^?:(){}[]~-_`
+	r := rand.New(rand.NewSource(1))
+	s := randstr.New(20, randstr.WithChars(chars), randstr.WithRand(r))
+	fmt.Println(s)
+	// Output: x42dht51DXM\S]}Y!q%_
+}
